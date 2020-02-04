572a296dea294975405af0ec90bb2a5c2b3653af
Walter Hoerman <whoerman@gmail.com>
2020-02-04 08:41:44 -0500

    setting up file structure to ease dividing tasks

---
+++ data.js
@@ -0,0 +1,2 @@
+//this will be where we save arrays from postman
+//also for setting up data arrays for use initially
---
+++ weatherjs.js
@@ -0,0 +1,2 @@
+//takes goecoded data and runs it though AJAX for weather
+//whole page should be wrapped in a function
