c1fd0a44dce8bff332535edbab4354a901223561
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-02-05 20:39:14 -0500

    open cage api

--- weatherjs.js
+++ weatherjs.js
@@ -1,2 +1,49 @@
 //takes goecoded data and runs it though AJAX for weather
 //whole page should be wrapped in a function
+apiKey = "&key=16fa0f1560a34557aeefa93881a42dfb";
+
+
+function buildQuery(city) {
+
+
+
+    
+    let queryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + city + apiKey + "&language=en&pretty=1";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        apiKey = "&key=16fa0f1560a34557aeefa93881a42dfb";
+        console.log(response);
+        
+
+        let getWeb = queryURL + apiKey;
+
+        console.log(city);
+        
+        console.log(response.results["0"].geometry);
+
+        let latCode = (response.results["0"].geometry.lat);
+        console.log(latCode);
+
+        let lngCode = (response.results["0"].geometry.lng);
+        console.log(lngCode);
+        
+
+
+
+
+
+        return getWeb;
+    });
+}
+
+
+
+$("#run-search").on("click", function () {
+    event.preventDefault();
+    let city = $("#search-term").val().trim();
+    buildQuery(city);
+});
+
