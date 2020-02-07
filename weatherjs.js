eedb2003fe7ce95886fe8a00e5ada3d717609784
Walter Hoerman <whoerman@gmail.com>
2020-02-07 16:15:37 -0500

    flexible weather design almost completed

--- weatherjs.js
+++ weatherjs.js
@@ -3,7 +3,7 @@
 
 let tempArray = [];
 let conditionArray = [];
-let windArray =[];
+let windArray = [];
 let fractionC = null;
 let clearPercent = null;
 let fractionR = null;
@@ -13,6 +13,8 @@ let snowPercent = null;
 let coldtemp = null;
 let hottemp = null;
 let tempAverage = null;
+let windMax = null;
+let maxWindSpeed = 0;
 
 
 let latitude = "43.072";
@@ -23,56 +25,109 @@ function processWeatherData() {
     checkConditions();
     checkTemps();
     checkWind();
-    weatherReport();
     weatherPlanner();
 }
 processWeatherData();
-
-function weatherReport() {
-    console.log("Your Weather Packing Planner!")
-    console.log("In the next 5 days:")
-    console.log(`The temps will be between ${coldtemp} and ${hottemp}, with an average of ${tempAverage}.`)
-    if (tempAverage < 40) {
-        console.log("It is going to be cold! Make sure you bring a layers plus a hat and gloves.")
-    };
-    if (tempAverage > 70) {
-        console.log("Awesome! Beach weather!");
-    };
-    if (clearPercent > 0) {
-        console.log(`It will be clear ${clearPercent} percent of the time,`);
-        if (tempAverage < 40) {
-            console.log("but cold, so only bring sunscreen if you are going skiing.");
-        };
-        if (tempAverage > 60) {
-            console.log("don't forget to bring sunscreen!.");
-        };
-    };
-    if (rainPercent > 0) {
-        console.log(`It will rain ${rainPercent} percent of the time, so bring a raincoat or umbrella.`);
-    };
-    if (rainPercent = 0) {
-        console.log(`yay! It is not going to rain!`);
-    };
-    if (snowPercent > 0) {
-        console.log(`It will snow ${snowPercent} percent of the time, so bring your winter gear!`);
-    };
-    if (snowPercent = 0) {
-        console.log(`yay! It is not going to snow!`);
-    };
-
-};
 
 function weatherPlanner() {
     $(".weather").empty();
     $plannerDiv = $("<div>");
     $header = $("<h4>");
-    $header.text("Your Short Break Weather Packing Planner!")
+    $header.text(" Your Short Break Weather Packing Planner! ")
     $plannerDiv.append($header);
+    $headericon = $("<i>")
+    $headericon.addClass("fas fa-suitcase-rolling");
+    $header.prepend($headericon);
     $subheader = $("<h5>");
-    $subheader.text("Plan your packing based on the weather! ")
-    
-
+    $subheader.text(`Plan your packing based on the weather! For the next 5 days in ${cityName}, here are the weather details:`);
+    $plannerDiv.append($subheader);
+    $headericon = $("<i>")
+    $headericon.addClass("fas fa-luggage-cart");
+    $header.append($headericon);
     //temperature discussion
+    $tempText = $("<h5>");
+    $tempText.append(` The temps will be between ${coldtemp} and ${hottemp}, with an average of ${tempAverage}.`);
+    if (tempAverage < 40) {
+        $tempText.prepend(` It is going to be cold in ${cityName}! Make sure you bring a layers plus a hat and gloves. `);
+    };
+    if (tempAverage > 70) {
+        $tempText.prepend(" Awesome! Beach weather!");
+    };
+    $tempicon = $("<i>")
+    $tempicon.addClass("fas fa-temperature-low");
+    $tempText.prepend($tempicon);
+    $plannerDiv.append($tempText);
+
+    //wind discussion
+    $windText = $("<h5>");
+    if (maxWindSpeed < 15) {
+        $windText.text(` The wind should be fairly calm. `)
+    };
+    if (maxWindSpeed > 15) {
+        $windText.text(` It will be occasionally windy. `)
+    };
+    $windText.append(` The maximum wind will be ${maxWindSpeed} mph.`);
+    $windicon = $("<i>")
+    $windicon.addClass("fas fa-wind");
+    $windText.prepend($windicon);
+    $plannerDiv.append($windText);
+
+    //clear skies discussion
+    $clearText = $("<h5>");
+    $clearText.text(` Sun report: It will be clear ${clearPercent} percent of the time,`);
+    let intClearPercent = parseInt(clearPercent);
+    if (intClearPercent = 0) {
+        $clearText.text(`Sadly, there will be no clear skies the entire time.`)
+    };
+    if (tempAverage < 40) {
+        $clearText.append(` but cold, so only bring sunscreen if you are going skiing.`)
+    };
+    if (tempAverage > 40) {
+        $clearText.append(`don't forget to bring sunscreen!.`)
+    };
+
+    $clearicon = $("<i>")
+    $clearicon.addClass("fas fa-cloud-sun");
+    $clearText.prepend($clearicon);
+    $plannerDiv.append($clearText);
+
+    //precipitation discussion
+    $precipText = $("<h5>");
+    $precipText.text(` Precipition: `);
+    $precipText.append(`It will rain ${rainPercent}%  of the time you are in ${cityName}, so bring a raincoat or umbrella. `);
+    $precipText.append(`It will snow ${snowPercent} percent of the time, so bring your winter gear! `);
+    $rainicon = $("<i>")
+    $rainicon.addClass("fas fa-cloud-showers-heavy");
+    $precipText.prepend($rainicon);
+    $plannerDiv.append($precipText);
+
+    $linebreak= ("<br>");
+    $plannerDiv.append($linebreak);
+    $hrline= ("<hr>");
+    $plannerDiv.append($hrline);
+
+    //adding 5 day planner
+    $daysText = $("<h5>");
+    $daysText.text(` On a short break, you need to be flexible, and know the exact conditions. Here are the detailed Forecasts (every 3 hours):`);
+    $dateicon = $("<i>")
+    $dateicon.addClass("fas fa-calendar-day");
+    $daysText.prepend($dateicon);
+    $plannerDiv.append($daysText);
+    let n = 0;
+    for (i=0; i < 39; i++) {
+    $forecastText = $("<h6>");
+    let currentWeather = responseFutureSample.list[i].weather[0].main;
+    let gotTime = responseFutureSample.list[i].dt;
+    var time = moment.unix(gotTime).format("MM-DD=YYYY HH:mm");
+    let futureTemp = responseFutureSample.list[i].main.temp;
+    futureTemp = (futureTemp - 273.15) * (9 / 5) + 32;
+    futureTempF = parseInt(futureTemp);
+    $forecastText.text(` ${time}    ${currentWeather}   ${futureTempF}°`);
+    $ellicon = $("<i>")
+    $ellicon.addClass("fas fa-ellipsis-h");
+    $forecastText.prepend($ellicon);
+    $plannerDiv.append($forecastText);
+    };
 
     //putting the panner on the page
     $(".weather").append($plannerDiv);
@@ -108,16 +163,14 @@ function checkWind() {
         let currentWind = responseFutureSample.list[a].wind.speed;
         windArray.push(currentWind);
     };
-    console.log(windArray);
     //check for rain or snow or clear precentages
-    for (a = 0; a < conditionArray.length; a++) {
-        if (conditionArray[a] == "Rain") {
-
+    for (a = 0; a < windArray.length; a++) {
+        if (windArray[a] > maxWindSpeed) {
+            maxWindSpeed = windArray[a];
         };
-
     };
+    maxWindSpeed = parseInt(maxWindSpeed);
 };
-
 
 
 
@@ -125,7 +178,7 @@ function checkTemps() {
     for (i = 0; i < 40; i++) {
         //run through all the temps over the next 5 days and convert them to F and put in temp array
         let futureTemp = responseFutureSample.list[i].main.temp;
-        futureTemp = (futureTemp - 273.15)*(9/5) +32;
+        futureTemp = (futureTemp - 273.15) * (9 / 5) + 32;
         futureTemp = parseInt(futureTemp);
         tempArray.push(futureTemp);
     };
@@ -145,6 +198,7 @@ function checkTemps() {
     tempAverage = parseInt(totaltemp / (j + 1));
 
 };
+
 
 //get the current weather forecast information
 function todayWeather() {
