f5e30849fe89115b6110b7afe73b4ac658e2e63b
Sam Delay <samuel.delay@gmail.com>
2020-02-08 16:10:51 -0500

    local storage and input value variables

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ also to process the data back and forth to local storage
 
 $(document).ready(function () {
 
+    recentCities = [];
+
+    let getCities = function () {
+        recentCities = JSON.parse(localStorage.getItem("cityArray"));
+        if (!recentCities) {
+            localStorage.setItem("cityArray", JSON.stringify(recentCities));
+        }
+    };
+    getCities()
+
+
     let stateOptions = function () {
-        states.forEach(function (item, index) {
+        states.forEach(function (item) {
             let newOpt = $("<option>");
-            newOpt.attr("value", index);
+            newOpt.attr("value", item)
             newOpt.text(item);
             $(".stateSelect").append(newOpt);
         });
@@ -17,27 +28,27 @@ $(document).ready(function () {
     stateOptions()
 
     let monthOptions = function () {
-        month.forEach(function (item, index) {
+        month.forEach(function (item) {
             let newOpt = $("<option>");
-            newOpt.attr("value", index);
+            newOpt.attr("value", item)
             newOpt.text(item);
             $(".month").append(newOpt);
         });
     }
     monthOptions()
     let dayOptions = function () {
-        day.forEach(function (item, index) {
+        day.forEach(function (item) {
             let newOpt = $("<option>");
-            newOpt.attr("value", index);
+            newOpt.attr("value", item)
             newOpt.text(item);
             $(".day").append(newOpt);
         });
     }
     dayOptions()
     let yearOptions = function () {
-        year.forEach(function (item, index) {
+        year.forEach(function (item) {
             let newOpt = $("<option>");
-            newOpt.attr("value", index);
+            newOpt.attr("value", item)
             newOpt.text(item);
             $(".year").append(newOpt);
         });
@@ -46,8 +57,44 @@ $(document).ready(function () {
 
     $('select').formSelect();
 
-    $(".search").on("click", function() {
-        window.location.replace("location.html")
+
+    $(".search").on("click", function () {
+        if (($("select.month").val() === null) || ($("select.day").val() === null) || ($("select.year").val() === null) || ($("select.stateSelect").val() === null) || (!$("#input_text").val())) {
+            alert("you dummy");
+        }
+
+        let month = $("select.month").val();
+        let day = $("select.day").val();
+        let year = $("select.year").val();
+        let state = $("select.stateSelect").val();
+        console.log(month)
+        console.log(day)
+        console.log(year)
+        console.log(state)
+        let city = $("#input_text").val()
+        if (!recentCities) {
+            recentCities = [(`${city}, ${state}`)]
+        } else if (recentCities.length >= 0) {
+            recentCities.unshift(`${city}, ${state}`)
+        }
+        localStorage.setItem("cityArray", JSON.stringify(recentCities))
+        $("#input_text").val(" ")
+        // window.location.replace("location.html")
     })
+
+
+
+
+
+    $(".weatherBtn").on("click", function () {
+        console.log("I work!")
+    })
+
+    $(".eventBtn").on("click", function () {
+        console.log("I work!")
+    })
+
+
+
 });
 
