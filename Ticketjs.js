19f121564f3c401b5ad4d417481d1f5c1e6060b1
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-02-08 11:36:07 -0500

    initial ticket code

--- Ticketjs.js
+++ Ticketjs.js
@@ -1,2 +1,18 @@
 //takes geocode from googlejs.js and AjAXs's it though Ticket API
 //whole page should be wrapped in a function
+
+
+var cities = ["Boston","Miami"];
+var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + cities[0] + "?apikey=azaUxsQPC2NvNoM7ZPpJOHJ0xw2N0iqd";
+//var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=azaUxsQPC2NvNoM7ZPpJOHJ0xw2N0iqd";
+
+function abc(){
+    $.ajax({
+        url: queryURL,
+        method:"GET"
+    }).done(function (response){
+        console.log("Response: "+JSON.stringify(response));
+    })
+}
+
+abc();
