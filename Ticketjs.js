768ceca735be3750bf08769ca6933d8ed58920cb
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-02-10 19:23:30 -0500

    merge master

--- Ticketjs.js
+++ Ticketjs.js
@@ -1,2 +1,50 @@
 //takes geocode from googlejs.js and AjAXs's it though Ticket API
 //whole page should be wrapped in a function
+
+var city = “Boston”;
+var apiKey = “azaUxsQPC2NvNoM7ZPpJOHJ0xw2N0iqd”;
+var queryUrl = “https://www.ticketmaster.com/v3/events/search/?q=” + city + apiKey;
+$.ajax({
+    url: queryUrl,
+    method: “GET”
+}).then(function (ticketMaster) {
+    var eventsT = ticketMaster.events;
+    var text = [];
+    for (i = 0; i < 5; i++) {
+        text +=
+            “<div class=‘eventDisplay’><p>” +
+            eventsT[i].name.text +
+            “</p>” +
+            “<p><a href=” +
+            eventsT[i].url +
+            ” target=‘_blank’>” +
+            eventsT[i].url +
+            “</a></p></div>“;
+    }
+    $.ajax({
+        url: queryUrl,
+        method: “GET”
+    }).then(function (ticketMaster) {
+        console.log(ticketMaster);
+    })})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- openCage.js
+++ openCage.js
@@ -1,47 +1,30 @@
-$(document).ready(function () {
-    apiKey = "&key=16fa0f1560a34557aeefa93881a42dfb";
-
-    let recentCities = [];
-
-    function buildQuery(cityState) {
-
-        let queryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + dest + apiKey + "&language=en&pretty=1";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (responseCoords) {
-
-            apiKey = "&key=16fa0f1560a34557aeefa93881a42dfb";
-            let getWeb = queryURL + apiKey;
-            console.log(responseCoords.results["0"].geometry);
-
-            let latCode = (responseCoords.results["0"].geometry.lat);
-            console.log(latCode);
-
-            let lngCode = (responseCoords.results["0"].geometry.lng);
-            console.log(lngCode);
+//this will take the data from indexTest and geocode it//takes goecoded data and runs it though AJAX for weather
+//whole page should be wrapped in a function
+apiKey = "&key=16fa0f1560a34557aeefa93881a42dfb";
+let latCode;
+let lngCode;
 
 
+function getCoords(arr) {
 
-
-
-
-        })
-
-
-    }
-    $(".search").on("click", cityState, function() {
-        let dest = $("#input_text").val();
-
-
-        if(!$("#input_text").val()) {
-        alert("please enter a valid city");
+    if (!arr) {
         return;
-        
-        
-        buildQuery()
     }
-        return cityState;
-});
-destinationSearch()
-})
+
+    let str = arr[0]
+    let split = str.split(",")
+    let myCity = split[0].split(" ").join("%20")
+    let state = split[1].replace(" ", "")
+    let searchID = `${myCity}%2C%20${state}`
+
+    const queryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + searchID + apiKey + "&language=en&pretty=1"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        latCode = (response.results[0].geometry.lat);
+        lngCode = (response.results[0].geometry.lng);
+
+    });
+}
