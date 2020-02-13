f18813f2ffd4469894772b1e43085d6daf4fc63f
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-02-12 19:24:25 -0500

    update openCage

--- openCage.js
+++ openCage.js
@@ -1,37 +1,91 @@
-//this will take the data from indexTest and geocode it//takes goecoded data and runs it though AJAX for weather
-//whole page should be wrapped in a function
-// apiKey = "&key=16fa0f1560a34557aeefa93881a42dfb";
-// let latCode = null;
-// let lngCode = null;
+// DOM elements
 
+/*
+var test = "blake";
+var arrival = "arrival date test string";
+var departure = "departure date test string";
+var arrivalEl = 2020-03-03;
+var departureEl = 2020-03-03;
+var day2 = 2020-03-04;
+var date = new Date();          // Get current Date
+*/
 
-// function getCoords(arr) {
+var invoiceDate = document.querySelector("#invoiceDate").value;
+var days = Number(document.querySelector("#days").value);
+var dueDateElement = document.querySelector("#dueDate");
+var initialDepartureDate = document.querySelector("#invoiceDate").value;
+var day1;
+var day2;
+var day3;
+var day4;
+var day5;
+var eventDays;
 
-//     if (!arr) {
-//         return;
-//     }
+/*
+console.log("test variable = " + test);
+console.log("arrival variable = " + arrival);
+console.log("departure variable = " + departure);
+console.log("arrivalEL variable = " + arrivalEl);
+console.log("departureEL variable = " + departureEl);
+*/
 
-//     let str = arr[0]
-//     let split = str.split(",")
-//     let myCity = split[0].split(" ").join("%20")
-//     let state = split[1].replace(" ", "")
-//     let searchID = `${myCity}%2C%20${state}`
+/*
+function submitDates() {
+    document.getElementById("demo").innerHTML = test;
+    document.getElementById("aDate").innerHTML = arrival;
+    document.getElementById("dDate").innerHTML = departure;
+    arrivalEl = document.getElementById('arrivalForm').value;
+    departureEl = document.getElementById('departureForm').value;
+    document.getElementById("aDate").innerHTML = arrivalEl;
+    document.getElementById("dDate").innerHTML = departureEl;
+console.log("test variable = " + test);
+console.log("arrival variable = " + arrival);
+console.log("departure variable = " + departure);
+console.log("arrivalEL variable = " + arrivalEl);
+/* console.log("departureEL variable = " + departureEl); */
 
-//     const queryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + searchID + apiKey + "&language=en&pretty=1"
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
+document.querySelector("#addDays").addEventListener("click", function () {
+    invoiceDate = document.querySelector("#invoiceDate").value;
+    days = Number(document.querySelector("#days").value);
+    dueDateElement = document.querySelector("#dueDate");
+    initialDepartureDate = document.querySelector("#invoiceDate").value;
 
-//         latCode = (response.results[0].geometry.lat);
-//         lngCode = (response.results[0].geometry.lng);
-//         console.log(latCode);
-//         console.log(lngCode);
-//         futureWeather();
-//     });
-// }
+    if (!isNaN(days) && invoiceDate.length) {
+        console.log("1st invoice date: " + invoiceDate);
+        invoiceDate = invoiceDate.split("-");
+        console.log("invoice date after split " + invoiceDate);
+        invoiceDate = new Date(invoiceDate[0], invoiceDate[1] - 1, invoiceDate[2]);
+        console.log("invoice date after new date: " + invoiceDate);
+        day1 = invoiceDate;
+        console.log("day 1 date: " + invoiceDate);
 
+        invoiceDate.setDate(invoiceDate.getDate() + 1);
+        day2 = invoiceDate;
+        console.log("day 2 date: " + invoiceDate);
 
+        invoiceDate.setDate(invoiceDate.getDate() + 1);
+        day3 = invoiceDate;
+        console.log("day 3 date: " + invoiceDate);
 
+        invoiceDate.setDate(invoiceDate.getDate() + 1);
+        day4 = invoiceDate;
+        console.log("day 4 date: " + invoiceDate);
 
+        invoiceDate.setDate(invoiceDate.getDate() + 1);
+        day5 = invoiceDate;
+        console.log("day 5 date: " + invoiceDate)
 
+        eventDays = [day1, day2, day3, day4, day5];
+        console.log("event dates: " + eventDays);
+
+        invoiceDate.setDate(invoiceDate.getDate() + days);
+        console.log("invoice date after set date and adding days: " + invoiceDate);
+
+        dueDateElement.valueAsDate = null;
+        dueDateElement.valueAsDate = invoiceDate;
+        console.log("initialDepartureDate = " + initialDepartureDate);
+        console.log("invoiceDate = " + invoiceDate);
+        console.log("dueDateElement = " + dueDateElement.value);
+        document.getElementById("dueDate").innerHTML = invoiceDate;
+    }
+});
